b197899c6d63b913b57d8975b4271b46199b845a
VienNguyen <chivien107@gmail.com>
2025-03-03 10:35:55 +0700

    fix error

--- app/type/index.ts
+++ app/type/index.ts
@@ -3,3 +3,31 @@ export type Authentication = {
   email: string;
   password: string;
 };
+
+export type ICourse = {
+  _id: string;
+  title: string;
+  description: string;
+  images: string[];
+  video: string;
+  courseDuration: string;
+  courseLevel: string;
+  author: string;
+  curriculum: ICurriculum[];
+};
+
+export type DetailStepCurriculum = {
+  lesson: string;
+  stepDescription: string;
+  stepDuration: string;
+};
+
+export type DetailCurriculum = {
+  name: string;
+  step: DetailStepCurriculum[];
+};
+
+export type ICurriculum = {
+  numOfCurriculum: string;
+  desCurriculum: DetailCurriculum;
+};
